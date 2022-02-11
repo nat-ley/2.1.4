265c1363e2818cf9dfe0c97ae90144a685500ab3
nat-ley <nat_ley@mail.ru>
2022-02-11 20:01:38 +0400

    2.1.4

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,6 @@
 package koschei.config;
 
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +14,10 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public static Egg6 getEgg() {
+        return new Egg6();
     }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Egg6 {
+    private Needle7 needle7;
+
+    @Autowired
+    public void setNeedle7(Needle7 needle7) {this.needle7 = needle7;}
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle7.toString();
     }
 }
